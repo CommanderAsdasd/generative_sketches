3731d365088fbb426922587bf35e9e918bc1c242
CommanderAsdasd <e1ectricwiz4rd@gmail.com>
2020-09-10 15:30:17 +0200

    Create basic_squashing_lines.glsl

---
+++ GLSL_sandbox/basic_squashing_lines.glsl
@@ -0,0 +1,15 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+#extension GL_OES_standard_derivatives : enable
+
+uniform float time;
+uniform vec2 mouse;
+uniform vec2 resolution;
+
+void main( void ) {
+	vec2 position = gl_FragCoord.xy/resolution.xy * sin(time);
+	float color = sin(time * position.x) * 3.;
+	gl_FragColor = vec4(vec3(sin(color),cos(time * time),sin(time)),1.);
+}
