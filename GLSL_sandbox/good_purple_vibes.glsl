ab21f734f61d78a9b12331fe9b3f87938e4ce21d
CommanderAsdasd <e1ectricwiz4rd@gmail.com>
2020-09-10 15:53:16 +0200

    Create good_purple_vibes.glsl

---
+++ GLSL_sandbox/good_purple_vibes.glsl
@@ -0,0 +1,16 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+#extension GL_OES_standard_derivatives : enable
+
+uniform float time;
+uniform vec2 mouse;
+uniform vec2 resolution;
+
+void main( void ) {
+	vec2 position = gl_FragCoord.xy/resolution.xy;
+	float color = sin(time + position.y - position.x) * 3. + position.y / position.x; // adding various  attribs of position changes
+	color -= cos(time + position.y - position.x) * 3. / sin(position.y * position.x); //
+	gl_FragColor = vec4(vec3(sin(color * 10.),cos(time),sin(time/time)),1.);
+}
